aa25c41ea311a2cc323a31c145441c229995dd33
Adeleke Ogunsona <110996523+adelekeogunsona@users.noreply.github.com>
2023-03-09 14:10:44 +0100

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import dis
 import marshal
 
-with open('hidden.pyc', 'rb') as f:
+with open('hidden_4.pyc', 'rb') as f:
     magic = f.read(4)  # Read the magic number
     timestamp = f.read(4)  # Read the timestamp
     code = marshal.load(f)  # Load the code object from the file
