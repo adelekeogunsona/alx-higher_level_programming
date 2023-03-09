a6a404aee9e34a273fbef4486f651742562da671
Adeleke Ogunsona <110996523+adelekeogunsona@users.noreply.github.com>
2023-03-09 14:08:42 +0100

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    all = dir(hidden_4)
-    for each in all:
-        if name[0:2] != "__":
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
