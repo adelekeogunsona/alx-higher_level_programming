2fa62577642d473786142c705e440eab43d55822
Adeleke <ogunsonaadedotun@gmail.com>
2023-03-13 17:02:45 +0100

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for element in my_list:
-            print("{:d}".format(element))
+def print_list_integer(my_list=[]):
+    for element in my_list:
+        print("{:d}".format(element))
